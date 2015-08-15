61d3b59275d4757ed2ffda4e59eee38c9a4a73be
ISHIDA Akio <ishida@coderium.com>
2015-08-15 16:40:03 +0900

    Change cache directory.

--- test.php
+++ test.php
@@ -2,14 +2,16 @@
 require 'vendor/autoload.php';
 
 $smartrunner = realpath("vendor/bin/smartrunner");
-$cache_file = ".smartrunner.cache";
+$cache_file = ".smartrunner/cache.json";
 
 @unlink($cache_file);
+@rmdir(dirname($cache_file));
 
 $out = [];
 exec("$smartrunner tests/CalcTest.php", $out);
 
-PHPUnit_Framework_Assert::assertTrue(is_readable($cache_file));
+PHPUnit_Framework_Assert::assertTrue(is_dir(dirname($cache_file)), dirname($cache_file) . " is not directory.");
+PHPUnit_Framework_Assert::assertTrue(is_readable($cache_file), "$cache_file is not readable.");
 PHPUnit_Framework_Assert::assertContains("CalcTest::test_add", implode($out));
 
 $out = [];
