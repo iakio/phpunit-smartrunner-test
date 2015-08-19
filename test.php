a71c6de18e42618afda5a90ab2e10cd7077333ca
ISHIDA Akio <ishida@coderium.com>
2015-08-19 20:18:14 +0900

    Add test for multiple TestCases.

--- test.php
+++ test.php
@@ -7,14 +7,17 @@ $cache_file = ".smartrunner/cache.json";
 @unlink($cache_file);
 @rmdir(dirname($cache_file));
 
-$out = [];
-exec("$smartrunner tests/CalcTest.php", $out);
-
+$out = []; exec("$smartrunner tests/CalcTest.php", $out);
 PHPUnit_Framework_Assert::assertTrue(is_dir(dirname($cache_file)), dirname($cache_file) . " is not directory.");
 PHPUnit_Framework_Assert::assertTrue(is_readable($cache_file), "$cache_file is not readable.");
 PHPUnit_Framework_Assert::assertContains("CalcTest::test_add", implode($out));
+PHPUnit_Framework_Assert::assertContains("OK (1 test, 1 assertion)", implode($out));
 
-$out = [];
-exec("$smartrunner src/Calc.php", $out);
+$out = []; exec("$smartrunner tests/OtherTest.php", $out);
+PHPUnit_Framework_Assert::assertContains("OtherTest::test_other", implode($out));
+PHPUnit_Framework_Assert::assertContains("OK (1 test, 1 assertion)", implode($out));
 
+$out = []; exec("$smartrunner src/Calc.php", $out);
 PHPUnit_Framework_Assert::assertContains("CalcTest::test_add", implode($out));
+PHPUnit_Framework_Assert::assertContains("OtherTest::test_other", implode($out));
+PHPUnit_Framework_Assert::assertContains("OK (2 tests, 2 assertions)", implode($out));
---
+++ tests/OtherTest.php
@@ -0,0 +1,8 @@
+<?php
+class OtherTest extends PHPUnit_Framework_TestCase
+{
+    function test_other() {
+        $calc = new Calc;
+        $this->assertEquals(30, $calc->add(10, 20));
+    }
+}
