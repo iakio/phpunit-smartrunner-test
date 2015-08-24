55e56019bc6873926c9b2de1f0c7f162f3543697
ISHIDA Akio <ishida@coderium.com>
2015-08-24 21:08:53 +0900

    Remove phpunit.xml.dist. Use config.json.

--- test.php
+++ test.php
@@ -2,22 +2,30 @@
 require 'vendor/autoload.php';
 
 $smartrunner = realpath("vendor/bin/smartrunner");
+$phpunit = realpath("vendor/bin/phpunit");
 $cache_file = ".smartrunner/cache.json";
+
+PHPUnit_Framework_Assert::assertTrue(class_exists('iakio\phpunit\smartrunner\SmartRunner'));
 
 @unlink($cache_file);
 @rmdir(dirname($cache_file));
 
-$out = []; exec("$smartrunner tests/CalcTest.php", $out);
+exec("$smartrunner init");
 PHPUnit_Framework_Assert::assertTrue(is_dir(dirname($cache_file)), dirname($cache_file) . " is not directory.");
+
+file_put_contents(".smartrunner/config.json", json_encode([
+    "phpunit" => "$phpunit --tap"
+]));
+$out = []; exec("$smartrunner run tests/CalcTest.php", $out);
 PHPUnit_Framework_Assert::assertTrue(is_readable($cache_file), "$cache_file is not readable.");
 PHPUnit_Framework_Assert::assertContains("CalcTest::test_add", implode($out));
-PHPUnit_Framework_Assert::assertContains("OK (1 test, 1 assertion)", implode($out));
+PHPUnit_Framework_Assert::assertContains("1..1", implode($out));
 
-$out = []; exec("$smartrunner tests/OtherTest.php", $out);
+$out = []; exec("$smartrunner run tests/OtherTest.php", $out);
 PHPUnit_Framework_Assert::assertContains("OtherTest::test_other", implode($out));
-PHPUnit_Framework_Assert::assertContains("OK (1 test, 1 assertion)", implode($out));
+PHPUnit_Framework_Assert::assertContains("1..1", implode($out));
 
-$out = []; exec("$smartrunner src/Calc.php", $out);
+$out = []; exec("$smartrunner run src/Calc.php", $out);
 PHPUnit_Framework_Assert::assertContains("CalcTest::test_add", implode($out));
 PHPUnit_Framework_Assert::assertContains("OtherTest::test_other", implode($out));
-PHPUnit_Framework_Assert::assertContains("OK (2 tests, 2 assertions)", implode($out));
+PHPUnit_Framework_Assert::assertContains("1..2", implode($out));
